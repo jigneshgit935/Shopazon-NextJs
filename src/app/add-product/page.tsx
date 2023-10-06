7c6a93ba356b63ed235fb8557a9bd2ea7719021a
Jignesh <jignesh.sharma.707@gmail.com>
2023-10-06 15:19:35 +0530

    add-product implemented

--- src/app/add-product/page.tsx
+++ src/app/add-product/page.tsx
@@ -1,42 +1,62 @@
+import FormSubmitButton from "@/components/FormSubmitButton";
+import { prisma } from "@/lib/db/prisma";
+import { redirect } from "next/navigation";
 
 export const metadata = {
-    title:"Add Products - Shopazon"
+  title: "Add Products - Shopazon",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price") || 0);
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: { name, description, imageUrl, price },
+  });
+  redirect("/");
 }
 
 export default function AddProductPage() {
-    return (
-        <div>
-            <h1 className="text-lg mb-3 font-bold">Add Product</h1>
-            <form>
-                <input
-                    className="input input-bordered w-full  mb-3"
-                    required
-                    name="name"
-                    placeholder="Name"
-                />
-                <textarea
-                    required
-                    name="desription"
-                    placeholder="Description"
-                    className="textarea textarea-bordered mb-3 w-full"
-                
-                />
-                <input
-                    className="input input-bordered w-full  mb-3"
-                    required
-                    name="imageUrl"
-                    placeholder="Image Url"
-                    type="url"
-                />
-                <input
-                    className="input input-bordered w-full  mb-3"
-                    required
-                    name="price"
-                    placeholder="Price"
-                    type="number"
-                />
-                <button type="submit" className="btn btn-primary btn-block ">Add Product</button>
-            </form>
-        </div>
-    )
+  return (
+    <div>
+      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form action={addProduct}>
+        <input
+          className="input-bordered input mb-3  w-full"
+          required
+          name="name"
+          placeholder="Name"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea-bordered textarea mb-3 w-full"
+        />
+        <input
+          className="input-bordered input mb-3  w-full"
+          required
+          name="imageUrl"
+          placeholder="Image Url"
+          type="url"
+        />
+        <input
+          className="input-bordered input mb-3  w-full"
+          required
+          name="price"
+          placeholder="Price"
+          type="number"
+        />
+        <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
+      </form>
+    </div>
+  );
 }
